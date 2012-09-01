53f2c73f2421dc7e0de6bdc12e13eb2f170999fe
kame1 <kame1@kame1.(none)>
2012-09-01 17:21:56 +0900

    Fix module declaration.

--- lib/cmd_tools/config.rb
+++ lib/cmd_tools/config.rb
@@ -1,36 +1,34 @@
-module CmdTools
-  module Config
-    require 'yaml'
-    require 'fileutils'
-    require 'ruby_patch'
-    extend ::RubyPatch::AutoLoad
+module ::CmdTools::Config
+  require 'yaml'
+  require 'fileutils'
+  require 'ruby_patch'
+  extend ::RubyPatch::AutoLoad
 
-    CONFIG_DIR = File.join(ENV['HOME'], '.config/cmd_tools')
-    CONFIG_FILE = File.join(CONFIG_DIR, 'config.yaml')
-    CONFIG_DEFAULT = {
-      emacs: ENV['ALTERNATE_EDITOR'] || 'emacs',
-    }
+  CONFIG_DIR = File.join(ENV['HOME'], '.config/cmd_tools')
+  CONFIG_FILE = File.join(CONFIG_DIR, 'config.yaml')
+  CONFIG_DEFAULT = {
+    emacs: ENV['ALTERNATE_EDITOR'] || 'emacs',
+  }
 
-    # (Re)load config file.
-    def self.load
-      @config = if File.size?(CONFIG_FILE) and File.readable?(CONFIG_FILE)
-                  CONFIG_DEFAULT.merge(YAML.load_file(CONFIG_FILE))
-                else
-                  FileUtils.mkdir_p(CONFIG_DIR)
-                  open(CONFIG_FILE, 'w'){|io|
-                    io.write(CONFIG_DEFAULT.to_yaml)
-                    io.flush
-                  }
-                  CONFIG_DEFAULT
-                end
+  # (Re)load config file.
+  def self.load
+    @config = if File.size?(CONFIG_FILE) and File.readable?(CONFIG_FILE)
+                CONFIG_DEFAULT.merge(YAML.load_file(CONFIG_FILE))
+              else
+                FileUtils.mkdir_p(CONFIG_DIR)
+                open(CONFIG_FILE, 'w'){|io|
+        io.write(CONFIG_DEFAULT.to_yaml)
+        io.flush
+      }
+                CONFIG_DEFAULT
+              end
 
-      self
-    end
-
-    def self.emacs
-      @config[:emacs]
-    end
-
-    self.load
+    self
   end
+
+  def self.emacs
+    @config[:emacs]
+  end
+
+  self.load
 end
--- lib/cmd_tools/version.rb
+++ lib/cmd_tools/version.rb
@@ -1,3 +1,3 @@
 module CmdTools
-  VERSION = '2.0.2'
+  VERSION = '2.0.3'
 end
