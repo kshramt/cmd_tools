2c180a213353fb16090b5b79084ec8a81656c1e9
kshramt <thisisdummy@example.com>
2012-11-04 14:38:17 +0900

    Replace TAB by 8 spaces.

--- lib/cmd_tools/recommended_shell_settings.rb
+++ lib/cmd_tools/recommended_shell_settings.rb
@@ -7,20 +7,20 @@ alias emacs_stop='cmd_tools emacs_stop'
 case $(uname) in
     # emacs_launch do not work satisfactory on Mac.
     "Darwin")
-	function e(){
-	    files=$@
-	    for file in ${files}
-	    do
-		[ -e ${file} ] || touch ${file}
-	    done
-	    open -a /Applications/MacPorts/Emacs.app ${files}
-	}
-	alias em='/Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs --no-window-system --no-init-file'
-	;;
+        function e(){
+            files=$@
+            for file in ${files}
+            do
+                [ -e ${file} ] || touch ${file}
+            done
+            open -a /Applications/MacPorts/Emacs.app ${files}
+        }
+        alias em='/Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs --no-window-system --no-init-file'
+        ;;
     *)
-	alias e='cmd_tools emacs_launch --mode=gui'
-	alias em='cmd_tools emacs_launch --mode=cui'
-	;;
+        alias e='cmd_tools emacs_launch --mode=gui'
+        alias em='cmd_tools emacs_launch --mode=cui'
+        ;;
 esac
   EOS
 end
