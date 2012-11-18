f4b0829a68feeb34ddc5443c89ba502305c133d8
kshramt <thisisdummy@example.com>
2012-11-18 18:13:14 +0900

    Add scripts instead of aliases and functions.

--- lib/cmd_tools/recommended_shell_settings.rb
+++ lib/cmd_tools/recommended_shell_settings.rb
@@ -1,26 +1,6 @@
 module CmdTools
   RECOMMENDED_SHELL_SETTINGS = <<-EOS
 # CmdTools
-alias bak='cmd_tools backup'
-alias tsh='cmd_tools trash'
-alias emacs_stop='cmd_tools emacs_stop'
-case $(uname) in
-    # emacs_launch do not work satisfactory on Mac.
-    "Darwin")
-        function e(){
-            local files="$@"
-            for file in ${files}
-            do
-                [ -e ${file} ] || touch ${file}
-            done
-            open -a /Applications/MacPorts/Emacs.app ${files}
-        }
-        alias em='/Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs --no-window-system'
-        ;;
-    *)
-        alias e='cmd_tools emacs_launch --mode=gui'
-        alias em='cmd_tools emacs_launch --mode=cui'
-        ;;
-esac
+export PATH=${HOME}/.gem/ruby/1.9.1/gems/cmd_tools-#{CmdTools::VERSION}/bin/command:${PATH}
   EOS
 end
